029e1fcaa585c58e6a8046677a3038e866f00a60
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-07-27 00:48:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ second_number = 3
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient = first_number 
+quotient = first_number / second_number
 
 
 
